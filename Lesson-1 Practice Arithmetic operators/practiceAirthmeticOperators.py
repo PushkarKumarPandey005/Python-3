d76455c7b33ab93ab3601a12659136e3e5309d0d
PushkarKumarPandey005 <pushkarkumar121005@gmail.com>
2025-08-06 00:26:50 +0530

    Practice of Airthmetic operators

---
+++ Lesson-1 Practice Arithmetic operators/practiceAirthmeticOperators.py
@@ -0,0 +1,23 @@
+num1 = int(input("Enter first integer number: "))
+num2 = int(input("Enter second integer number: "))
+#Add tow numbers:
+print(f"Result is: {num1} + {num2} = ", num1 + num2)
+
+#Subtract two numbers:
+print(f"Result is: {num1} - {num2} = ", num1 - num2)
+
+#Multiplications of two numbers:
+print(f"Result is: {num1} * {num2} = ", num1 * num2)
+
+#Division of two numbers:
+print(f"Result is: {num1} / {num2} = ", num1 / num2)
+
+#Find remainder, Modulo division operator is using for finding remainder:
+print(f"Result is: {num1} % {num2} = ", num1 % num2)
+
+#find the power, this operator is used to find power of numbers, is used **:
+#Example 4**2, 5**3, 8**9, 10**10 etc.
+print(f"Result is: {num1**num2} = ", num1**num2)
+
+#Find the qutient (//), this operator is used in python to find qutient.
+print(f"Result is: {num1} // {num2} = ", num1 // num2)
