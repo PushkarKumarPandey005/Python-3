c632afa5a2fef3877f7f2c84f7b2cd6659fbbcf9
PushkarKumarPandey005 <pushkarkumar121005@gmail.com>
2025-08-05 23:24:28 +0530

    Add two Integer Numbers:

---
+++ Lesson-1 Practice/sumOf2Integer.py
@@ -0,0 +1,6 @@
+# take 2 inputs integers numbers from user.
+
+num1 = int(input("Enteger first integer number: "))
+num2 = int(input("Enter second integer nummber: "))
+print(f"Result is: {num1} + {num2} =", num1 + num2)
+
