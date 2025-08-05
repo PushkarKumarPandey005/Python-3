582fc8b0f8d0db1ce60346e4366f9a8763ff2751
PushkarKumarPandey005 <pushkarkumar121005@gmail.com>
2025-08-05 23:48:05 +0530

    Add two flaot numbers and print their results

---
+++ Lesson-1 Practice/floatAdd.py
@@ -0,0 +1,4 @@
+'''Add two float numbers and take input from users'''
+num1 = float(input("Enter first float number: "))
+num2 = float(input("Enter second float number: "))
+print(f"Addition of {num1} and {num2} = Result is: ", num1 + num2)
