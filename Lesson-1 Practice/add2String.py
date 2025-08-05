1b077a0efed5a1250f2a5d7d125c52ef54bec112
PushkarKumarPandey005 <pushkarkumar121005@gmail.com>
2025-08-06 00:00:50 +0530

    Concatinate two strings

---
+++ Lesson-1 Practice/add2String.py
@@ -0,0 +1,7 @@
+# Concatinate two string and take input from users.
+
+str1 = input("Enter first strings and second strings: ") 
+str2 = input("Enter first strings and second strings: ")
+
+
+print(f"Result is: {str1} + {str2} = ", str1 + str2)
