386410d73a0a7e6fc27be007960211d821953d0e
PushkarKumarPandey005 <pushkarkumar121005@gmail.com>
2025-08-06 01:45:01 +0530

    Find the greatest number

--- Lesson - 2 Relational or comparision Operators/findTheGreatestNumbers.py
+++ Lesson - 2 Relational or comparision Operators/findTheGreatestNumbers.py
@@ -1,1 +1,10 @@
-num
+# find the greatest number.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(f"{num1} is greatest number:")
+elif num2 > num1:
+    print(f"{num2} is  greatest number:")    
+else:
+    print("Numbers is equal:") 
