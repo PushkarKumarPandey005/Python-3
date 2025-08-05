eff5ffeb5dc2f7abd90cff48066cc5a4ab26b43b
PushkarKumarPandey005 <pushkarkumar121005@gmail.com>
2025-08-05 23:37:40 +0530

    Add two numbers which is first integer and secon float number and print result

---
+++ Lesson-1 Practice/add1integerAnd1floatNumbers.py
@@ -0,0 +1,6 @@
+'''Take 2 input from users, first number is integer and second 
+ number is float number and add these numbers and print results'''
+
+num1 = int(input("Enter first integer number: "))
+num2 = float(input("Enter second float number: "))
+print(f"Result is: {num1} + {num2} = ", num1 + num2)
