642f45949cac019f6d553508fd999b45a506299b
PushkarKumarPandey005 <pushkarkumar121005@gmail.com>
2025-08-06 10:55:07 +0530

    update

--- Lesson - 2 Relational or comparision Operators/checkPositiveOrNegetive.py
+++ Lesson - 2 Relational or comparision Operators/checkPositiveOrNegetive.py
@@ -2,5 +2,7 @@
 num = float(input("Enter the your number: "))
 if num > 0:
     print("Positive number: ")
+elif num == 0:
+     print("Nnumber is Zero: ")   
 else:
     print("Negetive Number: ")    
